755aa6ae626c1323ab3b6ccf5a598fe418370347
Görkem GÜLER <gorkemguler1996@gmail.com>
2022-06-15 17:56:18 +0300

    createItem sayfası hazırlanacak

---
+++ src/utils/data.js
@@ -0,0 +1,11 @@
+import i1 from '../img/i1.png'
+import f1 from '../img/f1.png'
+import c3 from '../img/c3.png'
+import fi1 from '../img/fi1.png'
+
+export const heroData = [
+    {id: 1, name: 'Icecream', decp: 'Chocolate & vanilla', price : '5.25' , imageSrc: i1},
+    {id: 2, name: 'Strawberries', decp: 'Fresh Strawberries ', price : '10.25' , imageSrc: f1},
+    {id: 3, name: 'Kebap', decp: 'Mixed Kebap Plate', price : '8.25' , imageSrc: c3},
+    {id: 4, name: 'Fish', decp: 'Fresh Fish', price : '9.25' , imageSrc: fi1},
+]
