ad9ebbec6d25923a18f05282089e50659832c409
Görkem GÜLER <gorkemguler1996@gmail.com>
2022-06-16 01:22:24 +0300

    itemları firebase yükle

--- src/utils/data.js
+++ src/utils/data.js
@@ -9,3 +9,42 @@ export const heroData = [
     {id: 3, name: 'Kebap', decp: 'Mixed Kebap Plate', price : '8.25' , imageSrc: c3},
     {id: 4, name: 'Fish', decp: 'Fresh Fish', price : '9.25' , imageSrc: fi1},
 ]
+
+export const categories = [
+    {
+        id: 1,
+        name: "Chicken",
+        urlParamName: "chicken",
+    },
+    {
+        id: 2,
+        name: "Curry",
+        urlParamName: "curry",
+    },
+    {
+        id: 3,
+        name: "Rice",
+        urlParamName: "rice",
+    },
+    {
+        id: 4,
+        name: "Fish",
+        urlParamName: "fish",
+    },
+    {
+        id: 5,
+        name: "Fruits",
+        urlParamName: "fruits",
+    },
+    {
+        id: 6,
+        name: "Icecreams",
+        urlParamName: "icecreams",
+    },
+
+    {
+        id: 7,
+        name: "Soft Drinks",
+        urlParamName: "drinks",
+    },
+];
